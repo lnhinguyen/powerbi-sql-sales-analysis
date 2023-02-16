6f041c4de4d9de82a9be4e60e0b93e2beb92586a
NhiNguyen <113321447+lnhinguyen@users.noreply.github.com>
2023-02-16 16:45:55 +0800

    Add files via upload

---
+++ Output Data/Exported Data FACT_InternetSales_v2.sql
@@ -0,0 +1,35 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+	[ProductKey],
+    [OrderDateKey],
+    [DueDateKey],
+    [ShipDateKey],
+    [CustomerKey],
+    --  ,[PromotionKey]
+    --  ,[CurrencyKey]
+    --  ,[SalesTerritoryKey]
+    [SalesOrderNumber],
+    --  ,[SalesOrderLineNumber]
+    --  ,[RevisionNumber]
+    --  ,[OrderQuantity]
+    --  ,[UnitPrice]
+    --  ,[ExtendedAmount]
+    --  ,[UnitPriceDiscountPct]
+    --  ,[DiscountAmount]
+    --  ,[ProductStandardCost]
+    --  ,[TotalProductCost]
+    [SalesAmount]
+    --  ,[TaxAmt]
+    --  ,[Freight]
+    --  ,[CarrierTrackingNumber]
+    --  ,[CustomerPONumber]
+    --  ,[OrderDate]
+    --  ,[DueDate]
+    --  ,[ShipDate]
+FROM 
+	[dbo].[FactInternetSales]
+WHERE
+	LEFT (OrderDateKey,4) >= 2019
+ORDER BY
+	OrderDateKey ASC
